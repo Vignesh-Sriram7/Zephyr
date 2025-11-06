bc2ec1da32e069458965df780b08560ab52913f1
Vignesh <vigneshs2603@gmail.com>
2025-11-06 23:42:26 +0100

    Push of stm32f303re custom board setup

---
+++ stmf303r/board.cmake
@@ -0,0 +1,7 @@
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(jlink "--device=STM32F303RE" "--speed=4000")
+
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
