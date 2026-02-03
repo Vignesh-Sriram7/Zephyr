bd5b51183d13d56ffcdc4b722470ebb75cdbacef
Vignesh <vigneshs2603@gmail.com>
2026-02-04 00:03:02 +0100

    Complete Code

--- Ultrasonic_Radar/src/main.c
+++ Ultrasonic_Radar/src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/sys/time_units.h>
 
 #define MAX_TICKS 10000
 
@@ -23,6 +24,7 @@ int main()
     uint32_t start_time = 0;
     uint32_t stop_time = 0;
     uint32_t duration = 0;
+    uint32_t pulse_ns = 0;
 
     if(!gpio_is_ready_dt(&trig))
         return 0;
@@ -45,42 +47,83 @@ int main()
 
     while(1){
 
-        
-        
-        // HC-SR04 code
-        gpio_pin_set_dt(&trig, 1);
-        k_busy_wait(10);
-        gpio_pin_set_dt(&trig, 0);
-
-        counter = 0;
-        while(gpio_pin_get_dt(&echo) == 0){
-            counter++;
-            if(counter>MAX_TICKS)
-                break;
-        }
-        start_time = k_cycle_get_32();  
-
-        counter2 = 0;
-        while(gpio_pin_get_dt(&echo) == 1){
-            counter++;
-            if(counter>MAX_TICKS)
-                break;
-        }
-        stop_time = k_cycle_get_32();
-        
-        if(counter < MAX_TICKS && counter2 < MAX_TICKS)
+        for(int angle = 0; angle<=180; angle+=5)
         {
-            duration = stop_time - start_time;#
-            uint64_t duration_us = (uint64_t)duration * 1000000 / CONFIG_SYS_CLOCK_TICKS_PER_SEC;
-            uint32_t distance_cm = (duration_us * 34) / 2000;
-            printk("Distance: %u cm\n", distance_cm);
-        }
-        else
-            printk("No object detected");
+            pulse_ns = 500000 + (angle * 2000000 / 180);
 
-       
+            pwm_set_pulse_dt(&servo, pulse_ns);
+            k_msleep(50);
+            // HC-SR04 code
+            gpio_pin_set_dt(&trig, 1);
+            k_busy_wait(10);
+            gpio_pin_set_dt(&trig, 0);
+
+            counter = 0;
+            while(gpio_pin_get_dt(&echo) == 0){
+                counter++;
+                if(counter>MAX_TICKS)
+                    break;
+            }
+            start_time = k_cycle_get_32();  
+
+            counter2 = 0;
+            while(gpio_pin_get_dt(&echo) == 1){
+                counter2++;
+                if(counter2>MAX_TICKS)
+                    break;
+            }
+            stop_time = k_cycle_get_32();
             
+            if(counter < MAX_TICKS && counter2 < MAX_TICKS)
+            {
+                duration = stop_time - start_time;
+                uint64_t duration_us = (uint64_t)duration * 1000000 / CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC;
+                uint32_t distance_cm = (duration_us * 34) / 2000;
+                printk("Angle: %d, Distance: %u cm\n", angle, distance_cm);
+            }
+            else
+                printk("No object detected");
+        }
         
+        for(int angle = 180; angle>=0; angle-=5)
+        {
+            pulse_ns = 500000 + (angle * 2000000 / 180);
+
+            pwm_set_pulse_dt(&servo, pulse_ns);
+            k_msleep(50);
+            // HC-SR04 code
+            gpio_pin_set_dt(&trig, 1);
+            k_busy_wait(10);
+            gpio_pin_set_dt(&trig, 0);
+
+            counter = 0;
+            while(gpio_pin_get_dt(&echo) == 0){
+                counter++;
+                if(counter>MAX_TICKS)
+                    break;
+            }
+            start_time = k_cycle_get_32();  
+
+            counter2 = 0;
+            while(gpio_pin_get_dt(&echo) == 1){
+                counter2++;
+                if(counter2>MAX_TICKS)
+                    break;
+            }
+            stop_time = k_cycle_get_32();
+            
+            if(counter < MAX_TICKS && counter2 < MAX_TICKS)
+            {
+                duration = stop_time - start_time;
+                uint64_t duration_us = (uint64_t)duration * 1000000 / CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC;
+                uint32_t distance_cm = (duration_us * 34) / 2000;
+                printk("Angle: %d, Distance: %u cm\n", angle, distance_cm);
+            }
+            else
+                printk("No object detected");
+        }
+            
+    k_msleep(wait_time_ms);    
     }
-    k_msleep(wait_time_ms);
+    
 }
