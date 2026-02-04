21320932e51ae16219650d4861af371dc9592e60
Vignesh <vigneshs2603@gmail.com>
2026-02-05 00:08:22 +0100

    Commented

--- Ultrasonic_Radar/src/main.c
+++ Ultrasonic_Radar/src/main.c
@@ -4,7 +4,7 @@
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/sys/time_units.h>
 
-#define MAX_TICKS 10000
+#define MAX_TICKS 10000 // Threshold to move on from waiting for the reply ping of the ultrasonic sensor
 
 
 // Sleep settings 
@@ -19,42 +19,47 @@ static const struct gpio_dt_spec echo = GPIO_DT_SPEC_GET(DT_ALIAS(hc_echo),gpios
 int main()
 {
     int ret;
-    uint32_t counter=0;
-    uint32_t counter2=0;
-    uint32_t start_time = 0;
-    uint32_t stop_time = 0;
-    uint32_t duration = 0;
-    uint32_t pulse_ns = 0;
+    uint32_t counter=0;     // Counter for the start ping
+    uint32_t counter2=0;    // Counter for the end ping
+    uint32_t start_time = 0;    // Timer start time    
+    uint32_t stop_time = 0;     // Timer stop time
+    uint32_t duration = 0;      // Total duration from sending the singal to getting it back
+    uint32_t pulse_ns = 0;      // Duty Cycle Pulse
 
+    // Check if the trigger is ready
     if(!gpio_is_ready_dt(&trig))
         return 0;
-
+    // Check if the echo is ready
     if(!gpio_is_ready_dt(&echo))
         return 0;
-
+    // Check if the servo is ready
     if(!pwm_is_ready_dt(&servo))
         return 0;
 
+    // Configure the trigger to output
     ret = gpio_pin_configure_dt(&trig, GPIO_OUTPUT);
     if(ret<0)
         return 0;
 
+    // Configure the echo to output
     ret = gpio_pin_configure_dt(&echo, GPIO_INPUT);
     if(ret<0)
         return 0;
 
+    // Set the trigger to 0 initially
     gpio_pin_set_dt(&trig, 0);
 
     while(1){
-        int echo_val = gpio_pin_get_dt(&echo);
-        printk("Echo Pin State: %d\n", echo_val);
+
+        // Loop over for the clock wise rotation of the servo motor
         for(int angle = 0; angle<=180; angle+=5)
         {
             pulse_ns = 500000 + (angle * 2000000 / 180);
 
             pwm_set_pulse_dt(&servo, pulse_ns);
             k_msleep(50);
-            // HC-SR04 code
+
+            // Fire the trigger pulse
             gpio_pin_set_dt(&trig, 1);
             k_busy_wait(10);
             gpio_pin_set_dt(&trig, 0);
