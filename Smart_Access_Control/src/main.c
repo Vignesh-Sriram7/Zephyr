18d4d7c5786c10d808ac365ae30977409f88a832
Vignesh <vigneshs2603@gmail.com>
2026-02-19 00:29:08 +0100

    Step 02: Headers and Macros

--- Smart_Access_Control/src/main.c
+++ Smart_Access_Control/src/main.c
@@ -14,3 +14,18 @@
 // Converts 128 Bit random string to a format esp32 understands
 #define BT_UUID_CUSTOM_SERVICE_VAL \
 	BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0)
+
+// UUID Universally Unique Identifier 
+// Uniquely identifies information without a central registration authority
+
+// Vendor Service UUID - custom. Like a folder which has everything realted to this app
+static const struct bt_uuid_128 vnd_uuid = BT_UUID_INIT_128(
+	BT_UUID_CUSTOM_SERVICE_VAL);
+
+// Vendor Encrypted UUID - Reading. The status characteristic
+static const struct bt_uuid_128 vnd_enc_uuid = BT_UUID_INIT_128(
+	BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef1));
+
+// Vendor Authenticated UUID - Writing. The action characteristic
+static const struct bt_uuid_128 vnd_auth_uuid = BT_UUID_INIT_128(
+	BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef2));
